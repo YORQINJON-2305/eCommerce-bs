78a81b749eb9fc0324736c95f016a7b6ebc3876d
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-09-17 11:57:04 +0500

    Four page is over

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ elBurger.addEventListener("click", function(){
 elClose.addEventListener("click", function(){
     elHeader.classList.remove("show")
 })
+
+$('.customer__carousel').slick({
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
